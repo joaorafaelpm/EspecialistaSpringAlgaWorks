ab8639168cf54ee6aaac24cc0f6c1239f771367e
João Rafael Pendezza Medeiros <joaorafaelpmed@gmail.com>
2025-12-17 09:38:37 -0300

    Configurando armazenamento de autorizações

--- src/main/java/com/algaworks/algafood_auth/AuthorizationServerConfig.java
+++ src/main/java/com/algaworks/algafood_auth/AuthorizationServerConfig.java
@@ -1,22 +1,30 @@
 package com.algaworks.algafood_auth;
 
-import jdk.jfr.Registered;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.http.MediaType;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.server.authorization.JdbcOAuth2AuthorizationService;
+import org.springframework.security.oauth2.server.authorization.OAuth2AuthorizationService;
 import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
+import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
 import org.springframework.security.oauth2.server.authorization.settings.OAuth2TokenFormat;
 import org.springframework.security.oauth2.server.authorization.settings.TokenSettings;
 import org.springframework.security.web.SecurityFilterChain;
@@ -28,6 +36,9 @@ import java.util.Arrays;
 
 @Configuration
 public class AuthorizationServerConfig {
+
+    @Autowired
+    private RedisConnectionFactory redisConnectionFactory;
 
     @Bean
     @Order(Ordered.HIGHEST_PRECEDENCE)
@@ -86,23 +97,44 @@ public class AuthorizationServerConfig {
                         .build())
                 .build();
 
+//        Authorization_Code grant_type + PKCE
 //        localhost:8081/oauth2/authorize?response_type=code&client_id=algafood-analytics&state=abc&redirect_uri=http://client-application
         RegisteredClient algafoodanalytics = RegisteredClient
                 .withId("2")
                 .clientId("algafood-analytics")
                 .clientSecret(passwordEncoder.encode("analytics123"))
-                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                .clientAuthenticationMethod(ClientAuthenticationMethod.NONE)
                 .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
-                .redirectUri("http://client-application")
+                .redirectUri("http://localhost:8082/callback")
                 .scope("WRITE")
                 .scope("READ")
+                .clientSettings(ClientSettings.builder()
+//                        Isso torna o PKCE obrigatório
+                        .requireProofKey(true)
+                        .build())
                 .tokenSettings(TokenSettings.builder()
                         .accessTokenFormat(OAuth2TokenFormat.REFERENCE)
                         .accessTokenTimeToLive(Duration.ofMinutes(30))
+
                         .build())
                 .build();
 
+//        Implicit grant_type, está depreciado, por isso eu não vou replicar, até para não perder tanto tempo como no password, mas esse estilo de autenticação segue o mesmo padrão do authorization_code, a diferença é que ele não retorna um código na url que nós usamos para acessar o token, ele retorna o token diretamente
+//        Esse método é muito menos seguro, não atoa foi depreciado, então é por isso que eu escolho não seguir com ele, mas a teoria é basicamente a mesma. E assim como a teoria é a mesma, a url é exatamente igual, a única diferença é no tipo de retorno, que passa de "code" para "token"
+//        localhost:8081/oauth2/authorize?response_type=token&client_id=algafood-web&state=abc&redirect_uri=http://client-application
+
         return new InMemoryRegisteredClientRepository(Arrays.asList(algafoodend , algafoodanalytics));
+    }
+
+    @Bean
+    public OAuth2AuthorizationService authorizationService(
+            JdbcTemplate jdbcTemplate,
+            RegisteredClientRepository registeredClientRepository) {
+
+        return new JdbcOAuth2AuthorizationService(
+                jdbcTemplate,
+                registeredClientRepository
+        );
     }
 
 
